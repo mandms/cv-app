2c6dd1e00b982c636b4f84c22d6480fae4a0ca48
mandms <for4ik45@mail.ru>
2024-10-30 15:17:48 +0300

    add .nojekyll

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <iframe src='/cv.pdf' className={styles.cv} />
+      <iframe src='./cv.pdf' className={styles.cv} />
     </div>
   );
 }
